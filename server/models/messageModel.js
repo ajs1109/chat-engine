bf82cb13ae4763210f40b672aeb83a365ecccfe8
Ajitesh Srivastava <ajitesh.sr615@gmail.com>
2023-08-08 02:47:58 +0530

    second commit

--- server/models/messageModel.js
+++ server/models/messageModel.js
@@ -2,17 +2,22 @@ import mongoose from "mongoose";
 
 const messageModel = mongoose.Schema(
   {
-    sender:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
-    content:{
-        
-    }
+    content: {
+      type: String,
+      trim:true,
+    },
+    chat: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Chat",
+    },
   },
   {
     timestamps: true,
   }
 );
 
-export const Chat = mongoose.model("Chat", chatModel);
+export const Chat = mongoose.model("Message", messageModel);
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,26 +2,28 @@ import mongoose from "mongoose";
 
 const userModel = mongoose.Schema(
   {
-    name:{
-        type:String,
-        required:true,
+    name: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true,
+    email: {
+      type: String,
+      required: true,
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+      type: String,
+      required: true,
+    },
+    pic: {
+      type: String,
+      required: true,
+      default:
+        "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper.png",
+    },
   },
-  pic:{
-    type:String,
-    required:true,
-    default:null,
-  },},
   {
     timestamps: true,
   }
 );
 
-export const User = mongoose.model("User", chatModel);
+export const User = mongoose.model("User", userModel);
