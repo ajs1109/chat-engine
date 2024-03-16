456d11bef9584e7c6fef1c057b432a9dfbe58c0d
ajs1109 <78317286+ajs1109@users.noreply.github.com>
2024-03-17 02:14:47 +0530

    Update ChatBox.tsx

--- client/src/components/ChatsComponent/ChatContent/ChatBox.tsx
+++ client/src/components/ChatsComponent/ChatContent/ChatBox.tsx
@@ -1,28 +1,27 @@
-import { Input } from "@/components/ui/input";
 import axios from "@/axios/axios";
-import ScrollableFeed from "react-scrollable-feed";
-import { SendHorizonal } from "lucide-react";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { activeChatProps } from "@/features/activeChat";
 import { Chat, User } from "@/features/chatSlice";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { SendHorizonal } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import Lottie from "react-lottie-player";
+import { useDispatch, useSelector } from "react-redux";
+import io from "socket.io-client";
+import typingg from "../../animations/typing.json";
 import {
   isLastMessage,
   isSameSender,
   isSameSenderMargin,
 } from "./messageLogic";
-import io from "socket.io-client";
-import Lottie from "react-lottie-player";
-import typingIndicator from "@/components/animations/typing.json";
 import { commonDataProps, sendNotification } from "@/features/commonData";
+// import ScrolableFeed from 'react-scrollable-feed'
 
 export interface currentChatProps {
   activeChat: activeChatProps;
 }
-export interface newMessageRecievedProps {
-  commonData: commonDataProps;
-}
+
 export interface messageProps {
   chat: Chat;
   content: string;
@@ -33,73 +32,81 @@ export interface messageProps {
   sender: User;
 }
 
+export interface newMessageReceivedProps {
+  commonData: commonDataProps;
+}
+
 const ENDPOINT = "https://chat-application-9r1e.onrender.com";
 
 var socket: any, selectedChatCompare: any;
 
 const ChatBox = () => {
   const dispatch = useDispatch();
-
+  const { result } = JSON.parse(localStorage.getItem("profile") || "");
+  const [loading, setLoading] = useState(false);
+  const [content, setContent] = useState("");
+  const [activeMessages, setActiveMessages] = useState<messageProps[] | []>([]);
   const { activeChat } = useSelector(
     (state: currentChatProps) => state.activeChat
   );
+
   const { notificationData } = useSelector(
-    (state: newMessageRecievedProps) => state.commonData
+    (state: newMessageReceivedProps) => state.commonData
   );
-  //@ts-ignore
-  const { result }: User = JSON.parse(localStorage.getItem("profile"));
-  const [loading, setLoading] = useState(false);
-  const [content, setContent] = useState("");
-  const [activeMessages, setactiveMessages] = useState<messageProps[] | []>([]);
+
   const [socketConnected, setSocketConnected] = useState(false);
   const [typing, setTyping] = useState(false);
-  const [istyping, setIsTyping] = useState(false);
+  const [isTyping, setIsTyping] = useState(false);
 
-  //socket io
   useEffect(() => {
     socket = io(ENDPOINT);
     socket.emit("setup", result);
     socket.on("connected", () => setSocketConnected(true));
     socket.on("typing", () => setIsTyping(true));
     socket.on("stop typing", () => setIsTyping(false));
-  }, []);
+  });
+
   const activeChatMessage = async () => {
     try {
       setLoading(true);
       const { data } = await axios.get(
-        `/messages/getmessages/${activeChat?._id}`
+        `/messages/getMessages/${activeChat?._id}`
       );
-      setactiveMessages(data);
+
+      setActiveMessages(data);
     } catch (err: any) {
       console.log(err.response.data);
     } finally {
       setLoading(false);
 
-      //join room
       socket.emit("join chat", activeChat?._id);
     }
   };
+
   useEffect(() => {
     activeChatMessage();
     selectedChatCompare = activeChat;
   }, [activeChat]);
 
   useEffect(() => {
-    socket.on("message recieved", (newMessageRecieved: messageProps) => {
-      if (
-        !selectedChatCompare ||
-        selectedChatCompare?._id !== newMessageRecieved.chat._id
-      ) {
+    socket.on(
+      "message received",
+      (newMessageReceived: messageProps) => {
         if (
-          notificationData === null ||
-          notificationData?.indexOf(newMessageRecieved) === -1
+          !selectedChatCompare ||
+          selectedChatCompare._id !== newMessageReceived.chat._id
         ) {
-          dispatch(sendNotification(newMessageRecieved));
+          //give notification
+
+          if (!notificationData?.includes(newMessageReceived)) {
+            dispatch(sendNotification(newMessageReceived));
+          }
+        } else {
+          setActiveMessages([...activeMessages, newMessageReceived]);
         }
-      } else {
-        setactiveMessages([...activeMessages, newMessageRecieved]);
-      }
-    });
+      },
+      []
+    );
   });
 
   const handlePress = async (e: any) => {
@@ -107,28 +114,24 @@ const ChatBox = () => {
       socket.emit("stop typing", activeChat?._id);
       let chatId = activeChat?._id;
       await axios
-        .post("/messages/sendmessage", { content, chatId })
+        .post("/messages/sendMessage", { content, chatId })
         .then((res) => {
-          console.log(res);
           socket.emit("new message", res.data);
-          setactiveMessages([...activeMessages, res.data]);
+          setActiveMessages([...activeMessages, res.data]);
         })
-        .catch((err) => {
-          console.log(err.response.data);
-        });
+        .catch((err) => console.log(err.response.data));
       setContent("");
     }
   };
 
-  //typing
-  const typingHandler = (e: any) => {
+  const typingHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setContent(e.target.value);
-
     if (!socketConnected) return;
     if (!typing) {
       setTyping(true);
       socket.emit("typing", activeChat?._id);
     }
+
     let lastTypingTime = new Date().getTime();
     var timerLength = 3000;
     setTimeout(() => {
@@ -141,76 +144,65 @@ const ChatBox = () => {
     }, timerLength);
   };
 
-  return (
-    <div className="bg-white flex flex-col rounded-md mt-2 w-full min-h-[85%] md:h-[700px] sm:h-[600px] lg:h-[600px] ">
-      <div className="w-full p-2 bg-muted flex flex-col min-h-[80%] justify-end overflow-y-auto no-scrollbar">
-        <ScrollableFeed>
-          {loading ? : (<div className="flex flex-col-reverse rounded-md z-10 w-full h-[620px] bg-[#101b21]"></div>) (     activeMessages &&
-            activeMessages.map((m, i) => (
-              <div
-                key={m._id}
-                className="flex space-x-1 basis-[80%]  items-center"
-              >
-                {(isSameSender(activeMessages, m, i, result._id) ||
-                  isLastMessage(activeMessages, i, result._id)) && (
-                  <Avatar className="ml-4 h-8 w-8 mr-2">
-                    <AvatarImage
-                      src={`http://localhost:5000/uploads/profilePic/${m.sender.pic}`}
-                      alt="@shadcn"
-                    />
-                    <AvatarFallback>
-                      {m.sender.name[0].toUpperCase()}
-                    </AvatarFallback>
-                  </Avatar>
-                )}
-                <div
-                  className={`w-fit font-medium rounded-3xl min-h-[90%] my-1 p-2 px-3`}
-                  style={{
-                    backgroundColor: `${
-                      m.sender._id === result._id ? `#ffffff` : `#141414`
-                    }`,
-                    color: `${
-                      m.sender._id === result._id ? "#141414" : "#ffffff"
-                    }`,
-                    marginLeft: isSameSenderMargin(
-                      activeMessages,
-                      m,
-                      i,
-                      result._id
-                    ),
-                  }}
-                >
-                  {m.content}
-                </div>
-              </div>
-            ))
-          )}
-        </ScrollableFeed>
-      </div>
-      <div className="relative">
-        {istyping ? (
-          <div className="absolute top-[-45px]">
-            {" "}
-            <Lottie
-              loop
-              animationData={typingIndicator}
-              play
-              style={{ width: 50, height: 50 }}
-            />
-          </div>
-        ) : (
-          <></>
-        )}
+  return !loading ? (
+    <div className=" flex flex-col-reverse rounded-md z-10 w-full h-[620px] bg-[#101b21]">
+      <div className="flex gap-2 w-full relative">
         <Input
-          className="text-base font-sans"
+          style={{ backgroundColor: "#202d35" }}
+          className="m-2 rounded-full text-[#b5babf]"
+          placeholder="Send Message..."
           value={content}
           onChange={(e) => typingHandler(e)}
-          placeholder="type..."
           onKeyDown={handlePress}
         />
-        <SendHorizonal className="h-6 w-6 absolute right-2 top-2 cursor-pointer" />
+
+        <Button
+          className="bg-[#0071d9] absolute bottom-[9px] h-[38px] w-[38px] right-[9px] rounded-full"
+          onClick={handlePress}
+        >
+          <SendHorizonal className="absolute left-[8px] z-10" />
+        </Button>
+      </div>
+      <div className="h-[620px] flex flex-col no-scrollbar">
+        {activeMessages.length > 0 &&
+          activeMessages.map((message: messageProps, index: number) => (
+            <div className="flex space-x-1 items-center" key={index}>
+              {(isSameSender(activeMessages, message, index, result._id) ||
+                isLastMessage(activeMessages, index, result._id)) && (
+                <Avatar className="ml-4 h-8 w-8 mr-2">
+                  <AvatarImage
+                    src={`http://localhost:5000/uploads/profilePicture/${message.sender.pic}`}
+                    alt="@shadcn"
+                  />
+                  <AvatarFallback>
+                    {message.sender.name[0].toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+              )}
+              <div
+                className="bg-[#0071d9] text-justify max-w-[50%] text-[#ffffff] min-w-[30px] rounded-lg py-auto px-2 pt-[0px] h-fit m-2"
+                style={{
+                  marginLeft: isSameSenderMargin(
+                    activeMessages,
+                    message,
+                    index,
+                    result._id
+                  ),
+                }}
+              >
+                <p className="max-w-full break-all">{message.content} </p>
+              </div>
+            </div>
+          ))}
+        {isTyping && (
+          <div className="w-fit ml-[60px] h-8 bg-[#0071d9] text-[#b5babf] rounded-xl px-2 pt-[2px] m-2">
+            <Lottie loop animationData={typingg} play className="w-12" />
+          </div>
+        )}
       </div>
     </div>
+  ) : (
+    <div className="flex flex-col-reverse rounded-md z-10 w-full h-[620px] bg-[#101b21]"></div>
   );
 };
 
